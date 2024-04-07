98af305448258d066ebcd3e27b99e8fc14937c89
Desgue <guedesgomero@gmail.com>
2024-04-07 21:47:36 +0100

    feat: create sql migration files

---
+++ backend/migrations/cleanup.sql
@@ -0,0 +1,1 @@
+TRUNCATE pants, shirts, shorts;
---
+++ backend/migrations/model.sql
@@ -0,0 +1,31 @@
+CREATE TYPE size AS ENUM ("XS", "S", "M", "L", "XL", "XXL");
+
+CREATE TABLE IF NOT EXISTS pants (
+    id SERIAL PRIMARY KEY,
+    model VARCHAR(255) NOT NULL,
+    size size NOT NULL,
+    color VARCHAR(255) NOT NULL,
+    price DECIMAL(6, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS shirts (
+    id SERIAL PRIMARY KEY,
+    model VARCHAR(255) NOT NULL,
+    size size NOT NULL,
+    color VARCHAR(255) NOT NULL,
+    price DECIMAL(6, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS shorts (
+    id SERIAL PRIMARY KEY,
+    model VARCHAR(255) NOT NULL,
+    size size NOT NULL,
+    color VARCHAR(255) NOT NULL,
+    price DECIMAL(6, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
