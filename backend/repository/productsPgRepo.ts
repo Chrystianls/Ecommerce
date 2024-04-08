913ffa44556bf6443fedc8330889626fff44b1c7
Desgue <guedesgomero@gmail.com>
2024-04-08 16:52:42 +0100

    feature: add getById query and refactor repository to class

--- backend/repository/productsPgRepo.ts
+++ backend/repository/productsPgRepo.ts
@@ -1,6 +1,5 @@
-import pg, { QueryResult } from 'pg';
+import pg, { Pool, QueryResult } from 'pg';
 import { Pants, TPants } from '../entities/entities';
-const {Pool} = pg;
 
 const pool = new Pool({
     host: "localhost",
@@ -12,19 +11,35 @@ const pool = new Pool({
 
 });
 
-
-export const getAllPantsQuery = async (): Promise<TPants[]> =>  {
-    try{
-        const start = Date.now()
-        const query = "SELECT id, model, size, price FROM pants"
-        const result = await pool.query({text: query})
-        const duration = Date.now() - start + "ms"
-        console.log("executed query", {statement: query, duration, rows: result.rowCount })
-        return result.rows
+export class PantsRepository {
+    pool: Pool
+    constructor(pool: Pool){
+        this.pool = pool
+    }    
+    getAll = async (): Promise<TPants[]> =>  {
+        try{
+            const start = Date.now()
+            const query = "SELECT id, model, size, price FROM pants"
+            const res = await this.pool.query({text: query})
+            const duration = Date.now() - start + "ms"
+            console.log("executed query", {statement: query, duration, rows: res.rowCount })
+            return res.rows
+        }
+        catch (error) {
+            console.error("error performing query", {error})
+            const emptyPants = {} as TPants
+            return  [emptyPants]
+        }
     }
-    catch (error) {
-        console.error("error performing query", {error})
-        const emptyPants = {} as TPants
-        return  [emptyPants]
+    getById = async (id: number) => {
+        const query = {
+            text: "SELECT id, model, size, price from pants WHERE id = $1",
+            values: [id]
+        }
+        const start = Date.now()
+        const res = await this.pool.query(query)
+        const duration = Date.now() - start + "ms"
+        console.log("executed query", {statement: query, duration, rows: res.rowCount })
+        return res.rows
     }
 }
