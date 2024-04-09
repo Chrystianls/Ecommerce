497634fb2b8ab1dbcfa528b8345efb1fc063f6d1
Desgue <guedesgomero@gmail.com>
2024-04-09 17:07:48 +0100

    feat: add .env to configure postgress

--- backend/api/server.ts
+++ backend/api/server.ts
@@ -1,18 +1,19 @@
 import express, { Application } from 'express';
 import pg from 'pg';
+import "dotenv/config";
 import { registerRoutes, shirtsRouter, shortsRouter } from './routes.js';
 import { PantsController } from '../controllers/pants.js';
 import { PantsRepository } from '../repository/productsPgRepo.js';
 
 const app: Application = express();
-const port = 3000;
+const port = process.env.SERVER_PORT || 3000;
 
 const db = new pg.Pool({
-    host: "localhost",
-    port: 5432,
-    database: "postgres",
-    user: "postgres",
-    password: "password",
+    host: process.env.POSTGRES_HOST || "localhost",
+    port: parseInt(process.env.POSTGRES_PORT!) || 5432,
+    database: process.env.POSTGRES_DB || "postgres",
+    user: process.env.POSTGRES_USER || "postgres",
+    password: process.env.POSTGRES_PASSWORD,
     max: 10,
 
 });
