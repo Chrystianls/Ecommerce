05696a1a3c8a2bfa1e24228aaba6301d6d006c9c
Chrystian <chrystian.eduardo01@gmail.com>
2024-04-09 10:58:12 -0300

    Browser router and routes added on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home/index";
 
 function App() {
+  // <HomePage />
   return (
     <>
-      <HomePage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
